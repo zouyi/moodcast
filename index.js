33788ee63d5cb2059cb0a29eb7a1985dd5d8a20c
Yi Zou <a01040683@se14l0124-22.ad.bcit.ca>
2018-05-17 17:18:28 -0700

    update

--- index.js
+++ index.js
@@ -9,8 +9,35 @@ var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 
 var leftButton = document.getElementById("leftArrow");
 var rightButton = document.getElementById("rightArrow");
 
-
 weekDay.innerHTML = weekdays[d.getDay()];
 curMonth.innerHTML = months[d.getMonth()];
 curDate.innerHTML = d.getDate();
+
+
+var dayButt = document.getElementById("dayButton");
+var weekButt = document.getElementById("weekButton");
+var indiButt = document.getElementById("indiButton");
+var dayline = document.getElementById("dayline");
+var weekline = document.getElementById("weekline");
+var indiline = document.getElementById("indiline");
+
+dayButt.addEventListener("mouseover", function(){
+    
+    dayline.style.animation = "underline 0.5s";
+    
+})
+
+weekButt.addEventListener("mouseover", function(){
+    
+    weekline.style.animation = "underline2 2s";
+    
+})
+
+indiButt.addEventListener("mouseover", function(){
+    
+    indiline.style.animation = "underline3 2s";
+    
+})
+
+
 

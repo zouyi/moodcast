1060cd60181dfc4a2c07fdbc67abcf0a8df9dd40
Zou Yi <intoner.dev@gmail.com>
2018-05-21 20:46:13 -0700

    new

--- index.js
+++ index.js
@@ -13,23 +13,169 @@ weekDay.innerHTML = weekdays[d.getDay()];
 curMonth.innerHTML = months[d.getMonth()];
 curDate.innerHTML = d.getDate();
 
-
 var dayButt = document.getElementById("dayButton");
+var statsButton = document.getElementById("statsButton");
 var weekButt = document.getElementById("weekButton");
 var indiButt = document.getElementById("indiButton");
 var dayline = document.getElementById("dayline");
 var weekline = document.getElementById("weekline");
 var indiline = document.getElementById("indiline");
 
-dayButt.addEventListener("mouseover", function(){
-    
-    
-})
+var submitButton = document.getElementById("submit");
 
-weekButt.addEventListener("mouseover", function(){
+statsButton.addEventListener("click", function(){
+
+  //alert("clicked");
+    $.ajax({url:"http://localhost:3000/dailyStats", success: function(result){
     
+        //console.log(typeof(result));
+      var userObj = JSON.parse(result);
+              //console.log(userObj.length);
+          //$("#display").html(result);
+          //newBar(result);
+      
+         var objlength = Object.keys(userObj.moodlist).length;
+    var selection = 0;
+
+      var happyCount = 0;
+      var sadCount = 0;
+      var scaredCount = 0;
+      var disgustCount = 0;
+      var angryCount = 0;
+      var exhaustCount = 0;
+  
+  
+     for(i=0;i<objlength;i++){        
+        selection = userObj.moodlist[i].mood;
+        
+        if(selection ==1 ){
+          happyCount++;
+        } else if(selection ==2){
+          
+          sadCount++;
+        }else if(selection ==3){
+          
+          scaredCount++;
+
+
+        }else if(selection ==4){
+        
+          disgustCount++;
+
+        
+        
+        }else if(selection ==5){
+          angryCount++;
+
+        
+        
+          }else if(selection ==6){
+            exhaustCount++;
+
+          }
+        
+      }
+              
+  new Chart(document.getElementById("newChart"), {
+    type: 'bar',
+    data: {
+      labels: ["Happy",
+        "Sad",
+        "Scared",
+        "Disgust", 
+        "Angry", 
+        "Exhausted"],
+      datasets: [{
+        backgroundColor: ["#E74C3C", "#52BE80","#F4D03F","#1F618D","#8A2BE2","#00FFFF"],
+        data: [happyCount, 
+               sadCount, 
+               scaredCount, 
+               disgustCount, 
+               angryCount, 
+               exhaustCount]
+      }]
+    },
+    options: {
+      title: {
+        display: true,
+        text: 'Bar Diagram'
+      }
+    }
+    });  
+    }});
+  
+ 
+});
+  
+
+      
+
+
+indiButt.addEventListener("click", function(){
+  
+    $.ajax({url:"http://localhost:3000/dailyStats", success: function(result){
     
-})
+        //console.log(typeof(result));
+      var userObj = JSON.parse(result);
+             // alert(userObj.length);
+              
+      var objlength = Object.keys(userObj.moodlist).length;
+      var statusLine;
+      var selection = 0;
+      
+      for(i=0;i<objlength;i++){
+        statusLine+=('<div>'+userObj.moodlist[i].name+' is feeling');  
+        
+        selection = userObj.moodlist[i].mood;
+        
+        if(selection ==1 ){
+          
+          statusLine= statusLine+' happy ';
+        } else if(selection ==2){
+          
+         statusLine= statusLine+' sad ';
+
+        }else if(selection ==3){
+          
+                     statusLine= statusLine+' scared ';
+
+
+      }else if(selection ==4){
+        
+         statusLine= statusLine+' disgust ';
+
+        
+        
+      }else if(selection ==5){
+           statusLine= statusLine+' angry ';
+
+        
+        
+      }else if(selection ==6){
+           statusLine= statusLine+' exhausted ';
+
+      }
+          statusLine= statusLine+' about ';
+          statusLine= statusLine+userObj.moodlist[i].content;
+      }
+              
+              
+         $("#display").html(statusLine);
+
+                //newBar(result);
+  
+      }});
+      
+});
+
+function newBar(result) {
+  
+   
+}
+
+
+
+
 
 indiButt.addEventListener("mouseover", function(){
     
--- server.js
+++ server.js
@@ -1,7 +1,28 @@
 const express = require("express");
 var app = express();
+var fs = require("fs");
+
+var bodyParser = require("body-parser");
+
+//make a new server
+
+//configure body-parser for express
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+
+var path = require('path');
+app.use('/', express.static(__dirname + '/'));
+
+app.use('/imgs', express.static(__dirname + '/imgs'));
+app.use('/css', express.static(__dirname + '/css'));
+app.use('/js', express.static(__dirname + '/js'));
+
 
 const port = process.env.PORT || 3000;
+
+var date = new Date();
+
 
 // Add headers
 app.use(function (req, res, next) {
@@ -23,10 +44,88 @@ app.use(function (req, res, next) {
     next();
 });
 
+app.post('/sendForm', function(req, res) {
+    var name = req.body.uname;
+    var emotion = req.body.emotion;
+    var content = req.body.ucontent;
+
+    var curDay = date.getDay();
+    var curDate = date.getDate();
+    var curMonth = date.getMonth();
+    var curYear = date.getFullYear();
+  
+  var userObj = {
+     "name": name,
+    "mood": emotion,
+    "content": content,
+    "day": curDay,
+    "date": curDate,
+    "month": curMonth,
+    "year": curYear
+    
+  };
+  
+  var obj = {
+    moodlist: []
+  };
+  
+  //obj.moodlist.push(userObj);
+  
+  
+  fs.readFile('moodlist.json', 'utf8', function readFileCallback(err, data){
+    if (err){
+        console.log(err);
+    } else {
+    obj = JSON.parse(data); //now it an object
+    obj.moodlist.push(userObj); //add some data
+    json = JSON.stringify(obj); //convert it back to json
+    fs.writeFile('moodlist.json', json, 'utf8'); // write it back 
+}});
+  
+  
+  
+
+  
+          res.send('<h1>Name:'+name+'</h1><br>'+
+                   '<h1>Emotion:'+emotion+'</h1><br>'+
+                   '<h1>Content:'+content+'</h1><br>'
+            
+                );
+
+    
+  /*
+    if(username == "Scooby" && pass == "Do"){
+        res.send('<h1>Success</h1>');
+      res.send(fname);
+    }else{
+        res.send('<h1>Login Incorrect</h1>');       
+    } */
+});
+
+
+
 
 app.get("/", function(req, resp){
     
-    resp.sendFile("index.html");
+     resp.sendFile(path.join(__dirname + '/index.html'));
+
+    
+})
+
+
+app.get("/dailyStats", function(req, resp){
+
+       
+  fs.readFile('moodlist.json', 'utf8', function readFileCallback(err, data){
+    if (err){
+        console.log(err);
+    } else {
+    obj = JSON.parse(data); //now it an object
+    json = JSON.stringify(obj);
+      resp.send(json);
+}});
+  
+
     
 })
 
